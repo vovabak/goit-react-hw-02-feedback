0351a41028f39ef45e83014fcaf8d831bcf331c3
Volodymyr Bakovskyi <vovabak84@gmail.com>
2022-10-22 18:47:41 +0300

    feedback widget done

---
+++ src/components/section/Section.jsx
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+import { SectionTitle } from './Section.styled';
+
+export const Section = ({ title, children }) => {
+    return <div>
+            <SectionTitle>{title}</SectionTitle>
+            {children}
+        </div>
+}
+
+Section.propTypes = {
+    title: PropTypes.string.isRequired,
+}
